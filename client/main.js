c24d69f3ece21c31121c9b9488a56e0fa868719f
Th1nkK1D <witheep@gmail.com>
2017-03-20 04:05:51 +0200

    Add method, basic fn working, on end buggy

--- client/main.js
+++ client/main.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 
@@ -8,12 +9,29 @@ angular.module('unicot', [
 ]).controller('mainController',['$scope','$http',function($scope,$http) {
     $scope.test = 'Hi there!';
 
-    $scope.add = function() {
+    /* Add song */
+    $scope.add = function(url) {
+      /*
       let url = 'https://www.youtube.com/watch?v=WXmTEyq5nXc';
       let jsonURL = 'http://www.youtube.com/oembed?url='+url+'&format=json';
       let ytObject;
 
       console.log(ytObject);
+      */
+
+      Meteor.call('add',url);
+    }
+
+    $scope.play = function() {
+      Meteor.call('play');
+    }
+
+    $scope.pause = function() {
+      Meteor.call('pause');
+    }
+
+    $scope.skip = function() {
+      Meteor.call('skip');
     }
 
     $scope.helpers({
--- imports/bot.js
+++ imports/bot.js
@@ -1,49 +1,88 @@
-import Meteor from 'meteor/meteor';
+import { Meteor } from 'meteor/meteor';
 
 import { Queue } from './queue.js';
 
-const Discord = require('discord.js');
-const ytdl = require('ytdl-core');
+if(Meteor.isServer) {
+    const Discord = require('discord.js');
+    const ytdl = require('ytdl-core');
 
-const token = 'MjgwMzY4NzI2NTk0MDI3NTIw.C6wpWw.f-ruHdcIvvv-5STgBUNb4AOvjwo';
-const client = new Discord.Client();
+    const token = 'MjgwMzY4NzI2NTk0MDI3NTIw.C6wpWw.f-ruHdcIvvv-5STgBUNb4AOvjwo';
+    const client = new Discord.Client();
 
-let streamSetting = { seek: 0, volume: 0.25 }
+    let streamSetting = { seek: 0, volume: 0.1 }
 
-let voiceChannel = null;
-let voiceConnection;
+    voiceChannel = null;
+    voiceConnection = null;
+    dispatcher = null;
 
-// Discord bot preparation
-client.on('ready', () => {
-  console.log('Unicot is running!');
-});
+    // Discord bot preparation
+    client.on('ready', () => {
+    console.log('Unicot is running!');
+    });
 
-client.on('message', m => {
-    if (m.content === '>>hi') {
-        if(voiceChannel == null) {
-            //Get voiceChannel
-            voiceChannel = client.channels.find(channel => channel.type === 'voice');
+    client.on('message', m => {
+        if (m.content === '>>hi') {
+            if(voiceChannel == null) {
+                //Get voiceChannel
+                voiceChannel = client.channels.find(channel => channel.type === 'voice');
 
-            //Join voiceChannel
-            voiceChannel.join()
-                .then(connection => {
-                    voiceConnection = connection;
-                    //const stream = ytdl('https://www.youtube.com/watch?v=WXmTEyq5nXc', {filter : 'audioonly'});
-                    //dispatcher = connection.playStream(stream, { seek: 0, volume: 0.25 });
-                })
-                .catch(console.error);
+                //Join voiceChannel
+                voiceChannel.join()
+                    .then(connection => {
+                        voiceConnection = connection;
+                    })
+                    .catch(console.error);
+            }
+        } else if(m.content === '>>bye') {
+            if(voiceChannel != null) {
+                //Leave voiceChannel
+                voiceChannel.leave();
+                voiceChannel = null;
+            }
         }
-    } else if(m.content === '>>bye') {
-        if(voiceChannel != null) {
-            //Leave voiceChannel
-            voiceChannel.leave();
-            voiceChannel = null;
-        }
+
+    });
+
+    //Enqueue function
+    enqueue = function() {
+        let currentSong = Queue.findOne({});
+
+        //stream song
+        let stream = ytdl(currentSong.url, {filter : 'audioonly'});
+        dispatcher = voiceConnection.playStream(stream,streamSetting);
+
+        dispatcher.on('end',function(){
+            dispatcher = null;
+            console.log('END');
+            enqueue();
+        });
     }
 
-});
+    // Meteor Methods called from front-end
+    Meteor.methods({
+        'add': function(url) {
+            Queue.insert({
+                "url": url,
+                "date": new Date()
+            });
+        },
+        'play': function() {
+            if(dispatcher == null) {
+                //No song playing
+                enqueue();
+            } else {
+                //Song is paused
+                dispatcher.resume();
+            }
+        },
+        'skip': function() {
+            dispatcher.end();
+            enqueue();
+        },
+        'pause': function() {
+            dispatcher.pause();
+        }
+    })
 
-// Meteor Methods called from front-end
-
-
-client.login(token);
+    client.login(token);
+}
